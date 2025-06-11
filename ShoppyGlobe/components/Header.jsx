136361678d924ad6f6a519deedb79285e7586da9
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-06-11 22:04:56 +0530

    Setting up vite and all the dependency

---
+++ ShoppyGlobe/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+    <ul className="Navigation">
+    <li>Home</li>
+    <li>Best Sellers</li>
+    <li>Men</li>
+    <li>Women</li>
+    <li>About Us</li>
+    </ul>)
+}
