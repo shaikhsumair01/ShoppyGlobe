f9855d3b4f2396c4d12fe2626845ce1321f91acc
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-07-10 16:58:58 +0530

    Protected the cart using jwt token

--- ShoppyGlobe/BackEnd/Router/cart.routes.js
+++ ShoppyGlobe/BackEnd/Router/cart.routes.js
@@ -1,8 +1,9 @@
 import { getCart, addCart, updateCart, deleteCart } from "./Controller/cart.controller.js"
+import { verifyToken } from "./Controller/user.controller.js"
 // getting all the routes form cart controllers
 export default function cartRoutes(app){
-app.get("/cart", getCart)
-app.post("/cart", addCart)
-app.put("/cart", updateCart)
-app.delete("/cart",deleteCart)
+app.get("/cart", verifyToken, getCart)
+app.post("/cart", verifyToken, addCart)
+app.put("/cart", verifyToken, updateCart)
+app.delete("/cart",verifyToken, deleteCart)
 }
